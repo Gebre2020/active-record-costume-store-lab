92a193883378ec089a08252bc7cee31910594662
Gebremeskel <gebre20122@gmail.com>
2021-01-27 20:40:20 -0600

    commit tree cf3cdd530307f5f062358a4b3925ae1df838e584

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,18 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+
+class Costume < ActiveRecord::Base
+    #def self.create_table
+    #     sql = <<-SQL
+    #     CREATE TABLE IF NOT EXISTS costumes (
+    #       id INTEGER PRIMARY KEY,
+    #       name TEXT,
+    #       price FLOAT,
+    #       image_url TEXT,
+    #       size TEXT
+    #     )
+    #     SQL
+    
+    #     ActiveRecord::Base.connection.execute(sql)
+    #   end 
+end 
